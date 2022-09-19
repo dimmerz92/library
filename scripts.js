f0ad8ccd7ab1cfe9edf53504501bbc35cb3f9b1b
dimmerz92 <andrew.weymes@outlook.com>
2022-09-19 12:59:55 +0800

    add delete functionality

--- scripts.js
+++ scripts.js
@@ -23,18 +23,22 @@ function Book(title, author, pages, read) {
 //Functions
 function addToLibrary() {
     //Adds Book objects to library array
+    if (!title.value || !author.value) {
+        //style changes
+        return;
+    }
     let readStatus = read.checked ? "Read" : "Not read";
     let book = new Book(title.value, author.value, pages.value, readStatus);
     if (bookComparator(book)) {
+        //div pop up error
         console.log("Book already in library");
-        return;
     } else {
         library.push(book);
         let row = table.insertRow();
         for (let key in book) {
             row.insertCell().innerText = book[key];
         };
-        row.insertCell().innerText = "delete icon"
+        row.insertCell().innerHTML = `<button onclick="deleteBook(this)">delete</button>`;
     };
 };
 
@@ -47,4 +51,15 @@ function bookComparator(book) {
         };
     });
     return isIn;
+};
+
+function deleteBook(e) {
+    //Deletes book from array and displayed table
+    let title = e.parentElement.parentElement.firstChild.textContent;
+    for (let i = 0; i < library.length; i++) {
+        if (library[i].title === title) {
+            library.splice(i, 1);
+        };
+    };
+    e.parentElement.parentElement.remove();
 };
